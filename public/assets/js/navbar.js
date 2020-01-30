28d1c5a3f6fe473189f16566a5a1ebc1bb260fd6
Manuel Neumayer <manuel.neumayer@campus.lmu.de>
2020-01-31 00:01:22 +0100

    Slight modifications in CSS and JSON

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector(".navbar");
 const navlinks = document.querySelectorAll(".navlinks a");
 
 window.onscroll = () => {
-    if (document.scrollingElement.scrollTop >= 200) {
+    if (document.scrollingElement.scrollTop >= 100) {
         navbar.classList.add("navbar-scrolled");
         
         navlinks.forEach((link) => {
