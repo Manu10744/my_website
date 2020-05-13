f71c79edac6c9190fc6e69e2b53f92f133d5388f
Manuel Neumayer <manuel.neumayer@campus.lmu.de>
2020-05-13 23:53:54 +0200

    Small design changes

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -4,15 +4,7 @@ const navlinks = document.querySelectorAll(".navlinks a");
 window.onscroll = () => {
     if (document.scrollingElement.scrollTop >= 100) {
         navbar.classList.add("navbar-scrolled");
-
-        navlinks.forEach((link) => {
-            link.style.color = "black";
-        })
     } else {
         navbar.classList.remove("navbar-scrolled");
-
-        navlinks.forEach((link) => {
-            link.style.color = "#eee";
-        })
     }
 };
