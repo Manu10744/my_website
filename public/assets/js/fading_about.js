222885b15ff13aed3e106d3a59d8f5e6ab4a69aa
Manuel Neumayer <manuel.neumayer@campus.lmu.de>
2020-06-15 16:36:15 +0200

    Clear for redesign

--- public/assets/js/fading_about.js
+++
@@ -1,9 +0,0 @@
-ScrollReveal().reveal(".landing-content-wrapper", { duration: 1000 });
-ScrollReveal().reveal(".code", { duration: 1000 });
-
-ScrollReveal().reveal(".timeline-intro h1", { duration: 2000, delay: 100 });
-ScrollReveal().reveal(".timeline", { duration: 2000, delay: 500 });
-        
-ScrollReveal().reveal(".personal-text", { duration: 1000 });
-ScrollReveal().reveal(".personal .personal-image", { duration: 1000, delay: 300 })
-ScrollReveal().reveal(".programming-languages", { duration: 1000 });
